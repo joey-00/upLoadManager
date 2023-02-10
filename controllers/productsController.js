de05be1b50107db1f3a992f6d74ff2cc83ffec1e
joey-00 <joseacevedo410@gmail.com>
2023-02-10 20:41:21 -0300

    clase 73 parte 2 incompleto

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,9 +3,35 @@ module.exports = {
         return res.render('addOneImage')
     },
     storeOneImage : (req,res) => {
-        return res.send(req.body)
+        const product = readJSON('productsOneImage.json');
+
+        const newProduct = {
+            id : products.length ? products[products-legth -1]-id +1 : 1,
+            name : req.body.name,
+            description : "lorem ipsum dolor amet sit",
+            image : req.file ? req.filename : null,
+        }
+
+        products.push(newProduct);
+
+        writeJSON('productsOneImage.json', products)
+
+        return res.redirect('/')
     },
     detailOneImage : (req,res) => {
-        return res.render('detailOneImage')
+        const products = readJSON('productsOneImage.json');
+        const product = products.find(product => product.id === +req.params.id)
+        return res.render('detailOneImage',{
+            ...product
+        })
+    },
+    addMultipleImages : (req,res) => {
+        return res.render('addMultipleImages')
+    },
+    storeMultipleImages : (req,res) => {
+        return res.send(req.body)
+    },
+    detailMultipleImages : (req,res) => {
+        return res.render('detailMultipleImages')
     }
 }
--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const {addOneImage,storeOneImage, detailOneImage} = require('../controllers/productsController')
+const {addOneImage,storeOneImage, detailOneImage, detailMultipleImages, addMultipleImages ,storeMultipleImages} = require('../controllers/productsController');
+
+const {uploadOneImage} = require('../middlewares/upload')
+
+const {uploadOnemage}  = require('../middlewares/upload');
 
 
 /* /products */
 router
   .get('/add-one-image',addOneImage)
-  .post('/add-one-image',storeOneImage)
-  .get('/detail-one-image',detailOneImage)
+  .post('/add-one-image', uploadOneImage.single('image'),storeOneImage)
+  .get('/detail-one-image/:id',detailOneImage)
+
+  .get('/add-multiple-images',addMultipleImages)
+  .post('/add-multiple-images',storeMultipleImages)
+  .get('/add-multiple-images/:id',detailMultipleImages)
 
 module.exports = router;
